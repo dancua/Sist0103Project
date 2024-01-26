ec207fd673baecfe20e8d83bf5b4ee5ea8f906ff
dan <rlaxodbs72@naver.com>
2024-01-26 17:20:21 +0900

    day0126

--- JavaProject/jdbcProject/src/day0125/QuizJdbcFoodOrder.java
+++ JavaProject/jdbcProject/src/day0125/QuizJdbcFoodOrder.java
@@ -23,7 +23,7 @@ public class QuizJdbcFoodOrder {
 				stmt = conn.createStatement();
 				rs = stmt.executeQuery(sql);
 				System.out.println("***배달 현황판***");
-				System.out.println("주문번호\t음식번호\t주문자\t\t음식명\t\t가격\t상호명\t상점위치\t주문자위치");
+				System.out.println("주문번호    음식번호\t주문자\t음식명\t\t가격\t상호명\t상점위치\t주문자위치");
 				System.out.println("----------------------------------------------------------------------------------------");
 				while(rs.next()) 
 				{
@@ -36,7 +36,7 @@ public class QuizJdbcFoodOrder {
 					String shop_loc = rs.getString("shop_loc");
 					String order_addr = rs.getString("order_addr");
 					
-					System.out.println(order_num + "\t\t" + fno + "\t\t" + order_name + "\t" + food_name + "\t" + price + "\t" + shop_name + "\t" + shop_loc + "\t" + order_addr);
+					System.out.println(order_num + "\t\t" + fno + "\t" + order_name + "\t\t" + food_name + "\t" + price + "\t" + shop_name + "\t\t" + shop_loc + "\t" + order_addr);
 					
 				}
 			} catch (SQLException e) {
